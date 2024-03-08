e2c457af20f5c4d84f054a026f689b8449eebdd7
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-08 18:01:19 +0000

    more work on validating passwords

--- validate_login.php
+++ validate_login.php
@@ -6,23 +6,25 @@ session_start(); // Start the session
 $conn = createConnection(); // Create the connection
 
 // Get the username and password from the form
-$name = $_POST['user'];
+$name = $_POST['username'];
 $pass = $_POST['password'];
 
-// Create the SQL query
-$search = "select * from users where username = '$name' && password ='$pass'";
+// Create and execute the SQL query
+$search = "select * from users where username = '$name'";
+$result = mysqli_query($conn, $search);
 
-// Execute the query
-$result = mysqli_query($con, $search);
+// Get the password from the result
+$row = mysqli_fetch_assoc($result);
+$hash = $row['password'];
 
 // Get the number of rows returned
 $num = mysqli_num_rows($result);
 
-if ($num == 1) { // If there is a user with the given username and password
+// If there is a user with the given username and password, and the password is correct, then log the user in
+if ($num == 1 && password_verify($pass, $hash)) {
 	$_SESSION['username'] = $name;
 	header('location:select.php'); // Redirect to the select.php page
 } else {
 	header('location:login.php'); // Otherwise, redirect to the login.php page
 }
-
 ?>
