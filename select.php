2fc2fdff42179d58c16bc93d2ea0fbbd71dbe0ae
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-03-30 19:54:40 +0000

    use redirectLogin() instead of reusing code

--- select.php
+++ select.php
@@ -1,5 +1,6 @@
 <?php
 include_once ("functions.php"); // Import createConnection(); function
+redirectLogin(); // Check if user is logged in
 $conn = createConnection(); // Create a connection to the database via createConnection(); 
 
 if (!$conn) { // Check connection
