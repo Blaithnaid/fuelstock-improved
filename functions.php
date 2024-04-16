c0015d016390eff6f188c2d4689372e4f97bfc07
Iarla Sparrow Burke <iarlaken@gmail.com>
2024-04-17 00:19:08 +0100

    adding cookies if $rememberme set in validateUser, and reworking checkLogin to support them

--- functions.php
+++ functions.php
@@ -9,22 +9,31 @@ function createConnection()
     // Create connection
     $connection = new mysqli($servername, $username, $password, $dbname);
     if (!$connection) {
-        die ("Connection failed: " . mysqli_connect_error());
+        die("Connection failed: " . mysqli_connect_error());
     }
     return $connection;
 }
 
 function checkLogin()
 {
-if (!isset($_SESSION["user_id"])) {
-        $_SESSION["noCredentials"] = "yes";
+    if (isset($_COOKIE["user_id"]) && isset($_COOKIE["username"])) { // First, check if the user has a cookie
+        // If they do, set the session variables to the cookie values
+        $_SESSION["user_id"] = $_COOKIE["user_id"];
+        $_SESSION["username"] = $_COOKIE["username"];
+        // If the user is an admin, set the session variable to true
+        if (isset($_COOKIE["isAdmin"]) && $_COOKIE["isAdmin"] == 1) {
+            $_SESSION["isAdmin"] = true;
+        }
+    } else if (!isset($_SESSION["user_id"])) {
+        // If the user doesn't have a cookie, check if they have a session
+        // If they don't have a session, redirect them to the login page
         header("Location: login.php");
         exit();
     }
 }
 
 // Function to validate and authenticate user login
-function validateUser($username, $password)
+function validateUser($username, $password, $rememberme)
 {
     $conn = createConnection();
     $query = "SELECT * FROM users WHERE username = '$username'";
@@ -37,11 +46,19 @@ function validateUser($username, $password)
             if ($row['isAdmin'] == 1) {
                 $_SESSION['isAdmin'] = true;
             }
+            if (isset($rememberme)) {
+                setcookie("user_id", $row['id'], time() + (86400 * 30), "/"); // 86400 = 1 day
+                setcookie("username", $row['username'], time() + (86400 * 30), "/");
+                if ($row['isAdmin'] == 1) {
+                    setcookie("isAdmin", 1, time() + (86400 * 30), "/");
+                }
+            }
             return true;
         }
-        return "A user with that username does not exist. Please try again.";
+        return "The entered password is incorrect. Please try again.";
         // returning two separate outcomes isn't very secure, but I just think it looks fancier
     }
+    return "A user with that username does not exist. Please try again.";
 }
 function usernameExists($username)
 {
